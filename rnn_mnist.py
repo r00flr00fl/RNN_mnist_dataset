007233a87b38818709d3088ccdae8240de4317b4
Yimmie Honrodt <yimmie@honrodt.de>
2019-05-11 11:15:05 +0200

    uncommented rnn algo, added comments

--- rnn_mnist.py
+++ rnn_mnist.py
@@ -19,34 +19,36 @@ timestamp = int(time.time())
 X_train = X_train / 255
 X_test = X_test / 255
 
-# tensorboard = TensorBoard(log_dir = f'{log_dir}/{timestamp}')
-#
-# model = Sequential()
-#
-# model.add(CuDNNLSTM(128, input_shape=(X_train.shape[1:]), return_sequences=True))
-# model.add(Dropout(0.2))
-#
-# model.add(CuDNNLSTM(128))
-# model.add(Dropout(0.2))
-#
-# model.add(Dense(32, activation='relu'))
-# model.add(Dropout(0.2))
-#
-# model.add(Dense(10, activation='softmax'))
-#
-# model.compile(optimizer = Adam(lr=0.001, decay=1e-6), loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
-# model.summary()
-#
-# model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test), callbacks=[tensorboard])
-# model.save(f'{model_dir}/{timestamp}.h5')
+tensorboard = TensorBoard(log_dir = f'{log_dir}/{timestamp}')
 
-model = load_model(f'{model_dir}/1557517060.h5')
+model = Sequential()
+
+model.add(CuDNNLSTM(128, input_shape=(X_train.shape[1:]), return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(CuDNNLSTM(128))
+model.add(Dropout(0.2))
+
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer = Adam(lr=0.001, decay=1e-6), loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.summary()
+
+model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test), callbacks=[tensorboard])
+model.save(f'{model_dir}/{timestamp}.h5')
+
+# model = load_model(f'{model_dir}/1557517060.h5')
 
 predictions = model.predict_classes(X_test, batch_size=32, verbose=1)
-comparison = np.nonzero(predictions != y_test)[0]      # gives indices of unequal values
-np.random.shuffle(comparison)
 
-# settings
+# finding out which predictions have been wrong
+comparison = np.nonzero(predictions != y_test)[0]       # gives indices of unequal values
+np.random.shuffle(comparison)                           # randomizing wrong predictions to be plotted
+
+# plot settings
 h, w = 10, 10        # for raster image
 nrows, ncols = 5, 4  # array of sub-plots
 figsize = [6, 8]     # figure size, inches
@@ -60,7 +62,7 @@ for i, axi in enumerate(ax.flat):
     # axi is equivalent with ax[rowid][colid]
     unequal_index = comparison[i]
     img = cv2.resize(X_test[unequal_index], (10,10))
-    axi.imshow(img, alpha=0.25)
+    axi.imshow(img)
     # get indices of row/column
     # write row/col indices as axes' title for identification
     axi.set_title(f'P: {predictions[unequal_index]}, Y: {y_test[unequal_index]}')
@@ -69,5 +71,6 @@ for i, axi in enumerate(ax.flat):
 plt.tight_layout(True)
 plt.show()
 
+# printing the evaluation score of the test set
 scores = model.evaluate(X_test, y_test, batch_size=128)
 print('\nTest result: %.3f loss: %.3f' % (scores[1]*100,scores[0]))
